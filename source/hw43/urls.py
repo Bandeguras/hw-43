3280ac8f684a6d56b9475fd0de9e2e888474e231
Said Saliev <repsaid@gmail.com>
2022-10-28 16:28:54 +0600

    configured the view and urls

--- source/hw43/urls.py
+++ source/hw43/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import index_view, first_view, second_view, third_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view),
+    path('first/', first_view),
+    path('second/', second_view),
+    path('third/', third_view)
+
 ]
